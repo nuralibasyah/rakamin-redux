e7399751025b7a00bf613b3c60ac7f7eeefaddee
TiofennyAngelina <71843405+TiofennyAngelina@users.noreply.github.com>
2024-02-12 09:27:41 +0700

    cici ngerjain store

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import store from './store';
+import CounterComponent from './CounterComponent';
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <CounterComponent />
+
+        </Provider>
+    );
+};
+
+export default App;

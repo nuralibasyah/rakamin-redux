ce9ba92cb5b686e14f69a235d4c8544b9c225426
Satria Abimayu <158445134+oddieee@users.noreply.github.com>
2024-02-12 14:46:51 +0700

    Update NoteForm.js

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,28 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {addNote} from '../actions/notesActions';
-import {View, TextInput, Button} from 'react-native';
+import { addNote } from '../actions/notesActions';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
 
-const NoteForm = () =>{
+const NoteForm = () => {
     const [noteText, setNoteText] = useState('');
     const dispatch = useDispatch();
 
-    const handleAddNote = () =>{
-        if(noteText.trim()!==''){
+    const handleAddNote = () => {
+        if (noteText.trim() !== '') {
             dispatch(addNote(noteText));
             setNoteText('');
         }
     };
 
-    return(
-        <View>
+    return (
+        <View style={styles.container}>
             <TextInput
-            placeholde="Type your note here"
-            value = {noteText}
-            onChangeText={(text)=> setNoteText(text)}/>
-            <Button title="Add Note" onPress={handleAddNote}/>
+                style={styles.input}
+                placeholder="Type your note here"
+                value={noteText}
+                onChangeText={(text) => setNoteText(text)}
+            />
+            <Button title="Add Note" onPress={handleAddNote} />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 10,
+        paddingHorizontal: 20,
+    },
+    input: {
+        marginBottom: 10,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+    },
+});
 
 export default NoteForm;

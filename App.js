1b0447fb631478d5b4bdf8a06d344c25c022b333
TiofennyAngelina <71843405+TiofennyAngelina@users.noreply.github.com>
2024-02-12 11:02:46 +0700

    cici

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {configureStore} from 'redux';
+import noteReducer from './src/reducers/notesReducer';
+import NoteForm from './src/components/NoteForm';
+import NoteList from './src/components/NoteList';
+import {View} from 'react-native';
+
+const store = configureStore(notesReducer);
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <View>
+        <NoteForm/>
+        <NoteList/>
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
